7a499087cdf4b6890ce0ba93ae96f3f9a6c63c2f
smeth194243 <smor7432@740s-pl-114.local>
2017-01-23 12:23:31 -0700

    Initial Commit

---
+++ FirstAfternoonCPlusPlus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FirstAfternoonCPlusPlus
+//
+//  Created by Morris, Seth on 1/23/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
