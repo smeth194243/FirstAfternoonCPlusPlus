cb5c60f264faed69ce3a52d7972c6abe2d718cb7
smeth194243 <smor7432@740s-pl-114.local>
2017-01-25 13:07:10 -0700

    Added the output questions

--- FirstAfternoonCPlusPlus/Controller/FirstController.cpp
+++ FirstAfternoonCPlusPlus/Controller/FirstController.cpp
@@ -14,4 +14,31 @@ using namespace std;
 void FirstController :: start()
 {
     cout << "words here please :D" << endl;
+    this->specialOutput();
+}
+
+void FirstController:: specialOutput()
+{
+    cout << "Hi how old are you?" << endl;
+    int age;
+    cin >> age;
+    cout << "you typed in " << age << endl;
+    
+    cout << "Type in your name :D" << endl;
+    string name;
+    cin >> name;
+    cout << "your name is " << name << " Oh, cool - :P" << endl;
+    cin.ignore();
+    cin >> name;
+    cout << name;
+    cout << "Type your name and age" << endl;
+    cin >> name >> age;
+    cout << "you typed " << name << " and " << age << endl;
+    cin >> name >> age;
+    cout << "you typed " << name << " and " << age << endl;
+    
+    cout << "Type in your full name " << endl;
+    cin.ignore();
+    getline(cin, name);
+    cout << name << endl;
 }
