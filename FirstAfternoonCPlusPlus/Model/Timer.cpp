ce5cd3fff359df16db4eff3388915aefa07f4c7f
smeth194243 <smor7432@740s-pl-106.local>
2017-01-31 13:36:05 -0700

    Worked on the Timer.cpp

--- FirstAfternoonCPlusPlus/Model/Timer.cpp
+++ FirstAfternoonCPlusPlus/Model/Timer.cpp
@@ -6,4 +6,38 @@
 //  Copyright © 2017 CTEC. All rights reserved.
 //
 
+#include <iostream>
 #include "Timer.hpp"
+
+using namespace std;
+
+Timer :: Timer()
+{
+    executionTime = 0;
+}
+
+void Timer:: resetTimer()
+{
+    executionTime = 0;
+}
+
+void Timer:: startTimer()
+{
+    executionTime = clock();
+}
+
+void Timer:: finishTimer()
+{
+    executionTime = clock() - executionTime;
+}
+
+long Timer:: getExectutionTimeInMicroseconds()
+{
+    return executionTime;
+}
+
+void Timer:: displayTimerInformation()
+{
+    cout << "It took this long to execute: " << executionTime<< endl;
+    cout << "That is this many seconds: " << executionTime/CLOCKS_PER_SEC << endl;
+}
